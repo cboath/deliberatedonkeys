aeb1c96662892a2ff389ea94dd95afbe921f5cb5
cboath <cteater@gmail.com>
2025-01-05 23:36:31 -0600

    theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,18 +2,78 @@ import { createTheme } from "@mui/material/styles";
 
 const palette = {
     primary: {
-        main: "red",
-        light: "white",
-        dark: "black",
-        contrastText: "blue"
+        main: "#5A3652",
+        light: "#AD9BA9",
+        dark: "#2D1B29",
+        contrastText: "#FFFFFF"
+    },
+    secondary: {
+        main: "#339FF2",
+        light: "#99CFF9",
+        dark: "#1A5079",
+        contrastText: "#FFFFFF"
+    },
+    backround: {
+        paper: "#F1F1FB",
+        default: "#eeeeee"
     }
 }
 
 const typography = {
-    fontFamily: "sans-serif",
+    fontFamily: "Lato, 'DM Sans', sans-serif",
     body1: {
         fontSize: "1rem",
         fontWeight: 500
+    },
+    body2: {
+        fontSize: "0.875rem",
+        fontWeight: 400,
+        lineHeight: 1.5
+    },
+    button: {
+        fontWeight: 500,
+        fontFamily: "Lato, 'DM Sans', sans-serif",
+    },
+    subtitle1: {
+        fontSize: "0.875rem",
+        fontWeight: 500,
+        fontFamily: "Lato, 'DM Sans', sans-serif",
+    },
+    subtitle2: {
+        fontSize: "0.875rem",
+        fontWeight: 500,
+        fontFamily: "Lato, 'DM Sans', sans-serif",
+        lineHeight: 1.5
+    },
+    h1: {
+        fontSize: "3.5rem",
+        fontWeight: 700,
+        fontFamily: "Lato, 'DM Sans', sans-serif",
+        lineHeight: 1.5
+    },
+    h2: {
+        fontSize: "2.5rem",
+        fontWeight: 700,
+        fontFamily: "Lato, 'DM Sans', sans-serif",
+        lineHeight: 1.5
+    },
+    h3: {
+        fontSize: "2rem",
+        fontWeight: 700,
+        fontFamily: "Lato, 'DM Sans', sans-serif",
+        lineHeight: 1.5
+    },
+    h4: {
+        fontSize: "1.75rem",
+        fontWeight: 700,
+        fontFamily: "Lato, 'DM Sans', sans-serif",
+        lineHeight: 1.5
+    },
+    h5: {
+        fontSize: "1.5rem",
+        fontWeight: 700,
+        fontFamily: "Lato, 'DM Sans', sans-serif",
+        lineHeight: 1.5
     }
 }
 
