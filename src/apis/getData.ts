f1c738d48e78c6df9b3577dc10f6fd3a9cdd18d0
cboath <cteater@gmail.com>
2025-02-10 19:25:19 -0600

    moar

--- src/apis/getData.ts
+++ src/apis/getData.ts
@@ -1,5 +1,26 @@
-import axios from "axios"
-import { DonkeyTable } from "../types"
+import axios from "axios";
+import { omit } from "ramda";
+import { DonkeyTable, TableRow } from "../types";
 
-export const getData = (endpoint: DonkeyTable["endpoint"]) => 
-    axios.get("/donkey-api/" + endpoint).then((res) => res.data)
+const uri = 'get this value from somewhere'
+
+export const getData = (endpoint: DonkeyTable["endpoint"], headers: object) => 
+    axios({
+        method: "get",
+        url: uri + endpoint,
+        headers,
+        withCredentials: true,
+    }).then((res) => res.data)
+
+export const createData = (
+    endpoint: DonkeyTable["endpoint"],
+    headers: object,
+    data: TableRow
+) => 
+    axios({
+        method: "post",
+        url: uri + endpoint,
+        data: omit(["id"], data),
+        headers,
+        withCredentials: true
+    }).then((res) => res.data)
